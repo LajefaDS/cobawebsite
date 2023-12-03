98de9e50730ec9b309b89c88d4ceedf42b2871e6
Lajefa <lajepang11@gmail.com>
2023-12-03 17:49:13 +0700

    kirim lagi

--- api/index.php
+++ api/index.php
@@ -10,3 +10,14 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
     echo json_encode(array('error' => 'Ga Bisa Masuk'));
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lajefa Djuda Sianturi
+</body>
+</html>
