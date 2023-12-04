e9168c8d5c7c20fbce89fa7aa51f0c4a648962c7
Lajefa <lajepang11@gmail.com>
2023-12-04 19:28:07 +0700

    Tes

--- api/index.php
+++ api/index.php
@@ -1,11 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Lajefa Djuda Sianturi
-</body>
-</html>
+<?php
+include "portofolio3.html";
+?>
+<?php
+if(isset($_GET['cari'])){
+    $cari = $_GET['cari'];
+    switch($cari){
+        case 'foto';
+        include "foto.html";
+        break;
+        case 'nib';
+        include "nib.html";
+        break;
+        case 'dmc';
+        include "dmc.html";
+        break;
+        case 'excel';
+        include "excel.html";
+        break;
+        case 'produk';
+        include "produk.html";
+        break;
+        default:
+        echo "Halaman Tidak Tersedia";
+        break;
+    }
+}else{
+    include "foto.html";
+}
+?>
+<?php
+include "kontak.html";
+?>
