b30739924de28705a3df4fb2c20708e4d769e60e
Lajefa <lajepang11@gmail.com>
2023-12-10 18:44:54 +0700

    Lagiii

--- api/index.php
+++ api/index.php
@@ -1,33 +1,33 @@
 <?php
-include "portofolio3.html";
+include "../portofolio3.html";
 ?>
 <?php
 if(isset($_GET['cari'])){
     $cari = $_GET['cari'];
     switch($cari){
         case 'foto';
-        include "foto.html";
+        include "../foto.html";
         break;
         case 'nib';
-        include "nib.html";
+        include "../nib.html";
         break;
         case 'dmc';
-        include "dmc.html";
+        include "../dmc.html";
         break;
         case 'excel';
-        include "excel.html";
+        include "../excel.html";
         break;
         case 'produk';
-        include "produk.html";
+        include "../produk.html";
         break;
         default:
         echo "Halaman Tidak Tersedia";
         break;
     }
 }else{
-    include "foto.html";
+    include "../foto.html";
 }
 ?>
 <?php
-include "kontak.html";
+include "../kontak.html";
 ?>
